64024c176ab46c5fda9687925c0c216859001eb8
ultimoakim <akandykim234@gmail.com>
2022-11-14 14:56:19 -0800

    added partial templates

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ def home(request):
     # In Django, we include .html file extensions, unlike rendering .ejs templates. See how the format is different?
     # All view (controller, if it was Express) functions need to define a positional parameter to accept the request object that Django will be passing in. This request object is very much like the req object in Express functions.
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
