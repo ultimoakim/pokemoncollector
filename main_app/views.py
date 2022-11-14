707d285753b1ace76a7ce2f5951224081c19be30
ultimoakim <akandykim234@gmail.com>
2022-11-14 14:36:40 -0800

    defined a main route (URL), defined the main_app home page URL, defined a view function, created a django template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    # In Django, we include .html file extensions, unlike rendering .ejs templates. See how the format is different?
+    # All view (controller, if it was Express) functions need to define a positional parameter to accept the request object that Django will be passing in. This request object is very much like the req object in Express functions.
+    return render(request, 'home.html')
--- pokemoncollector/urls.py
+++ pokemoncollector/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# Don't forget to add the "include" function to the import!
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Remember, an empty string '' represents the "starts with" path in Django. A bit different from Express, as you can see.
+    # Also, don't forget about the trailing backslash. That's another Django thing.
+    path('', include('main_app.urls')),
 ]
